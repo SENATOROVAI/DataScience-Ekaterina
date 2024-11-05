76f1dcaaf8bcd3c787e7dc1064db06ecb6a8b958
Katerina-finch <finch976@gmail.com>
2024-11-05 22:13:00 +0500

    converted from ipynb to py
    chapters 1-11

---
+++ Python/made-easy/test.py
@@ -0,0 +1,25 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     custom_cell_magics: kql
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+"""Test."""
+
+# %%
+1 + 1
+
+# %%
+2 + 2
+
+# %% [markdown]
+# ![image.png](attachment:image.png)
